1a1b8d13fe40486e34ad6f01cc7e92fa71a2cf47
Jason <jmaalegado@gmail.com>
2023-04-20 22:36:07 +1000

    Modified environment vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,1 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.openweathermap.org/data/2.5/',
-  apiKey: '516807e564fa7c9be2ea0f97085189fb',
-};
+export const environment = {};
